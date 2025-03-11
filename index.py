b7363d558d4b9b86d2a834d2a6f6aca2bf50b90a
Dev Igor <igorprogramador03@gmail.com>
2025-03-11 15:28:38 -0300

    a

--- index.py
+++ index.py
@@ -83,6 +83,11 @@ def academy():
     nome_usuario = session.get('nome')  # Obtém o usuário da sessão
     return render_template('academy.html', nome=nome_usuario)
 
+@app.route('/news')
+def news():
+    nome_usuario = session.get('nome')  # Obtém o usuário da sessão
+    return render_template('news.html', nome=nome_usuario)
+
 @app.route('/historico')
 def historico():
     nome_usuario = session.get('nome')  # Obtém o usuário da sessão
