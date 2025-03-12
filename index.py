01f8364e0998443b7f29022dae055e197b7203b6
Dev Igor <igorprogramador03@gmail.com>
2025-03-12 12:39:01 -0300

    a

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 import random 
 from datetime import datetime, timedelta
 
+
 app = Flask(__name__)
 app.secret_key = 'chave_secreta'  # Necessário para usar sessões
 
